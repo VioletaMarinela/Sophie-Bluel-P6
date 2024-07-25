a5c5bc6ac30575c57d0743a41583fd724d8dc193
marylutza <marylutza@yahoo.com>
2024-07-25 14:28:37 +0200

    ChangePath

---
+++ FrontEnd/assets/JS/index.js
@@ -0,0 +1,9 @@
+fetch('http://localhost:5678/api/works')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error('Il y a eu une erreur:', error));
