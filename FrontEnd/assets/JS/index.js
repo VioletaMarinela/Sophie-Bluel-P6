7d7fc95262180e13dbce5583ce0784e724c5ad10
marylutza <marylutza@yahoo.com>
2024-07-25 14:57:39 +0200

    Affichage Works

--- FrontEnd/assets/JS/index.js
+++ FrontEnd/assets/JS/index.js
@@ -1,9 +1,36 @@
-fetch('http://localhost:5678/api/works')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('Il y a eu une erreur:', error));
+
+
+
+async function init() {
+    let allworks = await getallworks();
+
+    displayworks(allworks);
+}
+
+init();
+
+
+function getallworks() {
+    return fetch('http://localhost:5678/api/works')
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Il y a eu une erreur:', error)
+        });
+}
+
+function displayworks(allworks) {
+    let gallerycontainer = document.querySelector(".gallery");
+    gallerycontainer.innerHTML = "";
+
+    for (const work of allworks) {
+        gallerycontainer.insertAdjacentHTML("beforeend",
+            `
+            <figure>
+				<img src="${work.imageUrl}" alt="${work.title}">
+				<figcaption>${work.title}</figcaption>
+			</figure>
+            `)
+    }
+}
